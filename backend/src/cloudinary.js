3916cf95f9042288abe155cc34a73b2eef3924b5
hernanofx <hernandorra@gmail.com>
2025-07-20 21:02:34 -0300

    variables privadas cloudinary

--- backend/src/cloudinary.js
+++ backend/src/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'duoarc5la',
-  api_key: '132162997293577',
-  api_secret: 's07zzyEOXGJHX5Z7mF6HQ_X_4FY',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
